fdf899c05b7f0bb9427bb33d0c4120cdc3584a29
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-07-11 13:47:14 -0300

    Documentando la App

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Gasto from './Gasto';
+import PropTypes from 'prop-types';
+
 
 const Listado = ({gastos}) => ( 
     <div className="gastos-realizados">
@@ -13,5 +15,9 @@ const Listado = ({gastos}) => (
         ))}
     </div>
 );
- 
+
+Listado.propTypes = {
+    gastos: PropTypes.array.isRequired
+}
+
 export default Listado;
--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import Error from './Error'
+import PropTypes from 'prop-types';
+
 
 const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => {
 
@@ -52,5 +54,11 @@ const Pregunta = ({guardarPresupuesto, guardarRestante, actualizarPregunta}) => 
         </Fragment>
      );
 }
- 
+
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired
+}
+
 export default Pregunta;
